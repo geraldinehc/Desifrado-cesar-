fc96e9be0643c82ffac83f364d83b3a0026dbaeb
geraldinehc <geraldine011214@gmail.com>
2017-10-10 17:14:18 -0500

    primer comentario

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 //crea una web que pida por medio de un prompt() una frase al usuario y devuelva el mismo mensaje
-var palabra = prompt("escribe una frase")
+var imput = prompt("escribe una texto")
+function caesar(texto) {
+ var output="";
+ var string = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPKRSTUVWXYZ"
+ for (var i=0; i<texto.length;i++){
+ 		output += String.fromCharCode(texto.charCodeAt(i));
+  };
 
-string frase1;
-
-string min ="abcdefghijklmnopqrstuvwxyz"
-string may ="ABCDEFGHIJKLMNOPKRSTUVWXYZ"
- for (init i=0;i<frace1.length();i++)
- 
+  return output
+}
+var string
