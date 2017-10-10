2f27ff5c2ee2572e21991f4df63031cdb8a51876
geraldinehc <geraldine011214@gmail.com>
2017-10-10 17:07:30 -0500

    mi proceso

---
+++ app.js
@@ -0,0 +1,9 @@
+//crea una web que pida por medio de un prompt() una frase al usuario y devuelva el mismo mensaje
+var palabra = prompt("escribe una frase")
+
+string frase1;
+
+string min ="abcdefghijklmnopqrstuvwxyz"
+string may ="ABCDEFGHIJKLMNOPKRSTUVWXYZ"
+ for (init i=0;i<frace1.length();i++)
+ 
